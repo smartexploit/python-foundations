8451f21d61914f9366d05a6e99a361bde31f0e5a
smartexploit <smartroyaldigitalservices@gmail.com>
2026-07-28 11:19:44 -0400

    Print Name and Mailing Address

---
+++ 01_basics/01_mailing_address.py
@@ -0,0 +1,12 @@
+"""Exercise 1: Mailing Address
+Displays full name and mailing address formatted cleanly.
+"""
+
+
+def main():
+    print("Faith Ogunlade")
+    print("12, Bhd Petim Sawmill, Adehun Ado-Ekiti, Ekiti State,Nigeria.")
+
+
+if __name__ == "__main__":
+    main()
